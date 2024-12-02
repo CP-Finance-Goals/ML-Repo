8394598a59590a59a878e3deaf055c758c6e1b3d
MUHAMADPRIANDA <3337220113@untirta.ac.id>
2024-12-03 02:10:41 +0700

    fix app.py

--- Fitur2/app.py
+++ Fitur2/app.py
@@ -88,5 +88,5 @@ def health():
 
 # Run the server
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 8080))
+        port = int(os.environ.get("PORT", 5000))
     uvicorn.run(app, host="0.0.0.0", port=port)

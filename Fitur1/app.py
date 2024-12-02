f314299412526b8a4597997bf362aa013c76b5f3
Ahmad Alfin Nur Hakim <ahmadalfin130804@gmail.com>
2024-12-02 23:47:46 +0700

    fix docker file and useless files

--- Fitur1/app.py
+++ Fitur1/app.py
@@ -121,4 +121,5 @@ def predict():
         return jsonify({'error': f'Terjadi kesalahan: {str(e)}'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
